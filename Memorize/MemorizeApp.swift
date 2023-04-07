b94265089cf78ab35599c758b394efbfdcd4eb5a
LuiArm <Luisarmendariz24@icloud.com>
2023-04-06 22:47:30 -0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by luis armendariz on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
