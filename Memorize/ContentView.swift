2cc62134324d49d8dc92ae59837774a7c775a2a4
LuiArm <Luisarmendariz24@icloud.com>
2023-04-07 23:22:03 -0600

    Add Card View

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,19 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🛻","🚓","🚝", "🚖"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack{
+            ForEach(emojis, id: \.self, content: {emoji in
+                CardView(content: emoji)
+            })
         }
-        .padding()
+        .padding(.horizontal)
+        .foregroundColor(.red)
+        
     }
 }
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = false
+    
+    var body: some View {
+        
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            }  else {
+                shape.fill().foregroundColor(.blue)
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
