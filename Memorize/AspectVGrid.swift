b36eec6001c2fa8ac02df97ac1f181d42ceaca7f
LuiArm <Luisarmendariz24@icloud.com>
2024-02-15 15:28:48 -0700

    aspect v grid

---
+++ Memorize/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Memorize
+//
+//  Created by luis armendariz on 2/15/24.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
