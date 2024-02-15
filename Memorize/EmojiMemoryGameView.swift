19d50a1fab00a0bbb782850cb1a097bed1bc03ce
LuiArm <Luisarmendariz24@icloud.com>
2024-02-15 16:06:51 -0700

    aspect v grid, viewbuilder

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -72,40 +72,14 @@ struct EmojiMemoryGameView: View {
 //    }
     
     private var cards: some View {
-        
-        
-        return GeometryReader { geometry in
-            let gridItemSize = gridItemWidthThatFits(count: viewModel.cards.count, size: geometry.size, atAspectRatio: aspectRatio)
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
-                ForEach(viewModel.cards) { card in
+        AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) { card in
                     CardView(card)
-                        .aspectRatio(aspectRatio, contentMode: .fit)
                         .padding(4)
                         .onTapGesture {
                             viewModel.choose(card)
                         }
                 }
-            }
-        }
         .foregroundStyle(.orange)
-    }
-
-    func gridItemWidthThatFits(count: Int, size: CGSize, atAspectRatio aspectRatio: CGFloat) -> CGFloat {
-        let count = CGFloat(count)
-        var columnCount = 1.0
-        repeat {
-            let width = size.width / columnCount
-            let height = width / aspectRatio
-            
-            let rowCount = (count / columnCount).rounded(.up)
-            if rowCount * height < size.height {
-                return (size.width / columnCount).rounded(.down)
-            }
-            columnCount += 1
-            
-        } while columnCount < count
-        return min(size.width / count, size.height * aspectRatio)
-
     }
 }
 
@@ -121,7 +95,7 @@ struct CardView: View {
     }
     
     var body: some View {
-        let shape = RoundedRectangle(cornerSize: CGSize(width: 30, height:30))
+        let shape = RoundedRectangle(cornerSize: CGSize(width: 15, height:15))
         ZStack{
             Group {
                 shape.fill(.white)
