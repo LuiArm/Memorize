6094a66a6301543b11d70bab84b5306b4ea8c842
LuiArm <Luisarmendariz24@icloud.com>
2024-02-16 22:26:27 -0700

    card view

---
+++ Memorize/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by luis armendariz on 2/16/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
