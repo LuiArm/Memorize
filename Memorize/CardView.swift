50c81fd4ef8ff8556030e2be25f99dc9f240f0eb
LuiArm <Luisarmendariz24@icloud.com>
2024-02-17 17:14:48 -0700

    card view

--- Memorize/CardView.swift
+++ Memorize/CardView.swift
@@ -7,9 +7,29 @@
 
 import SwiftUI
 
+//Card view to make it reusable
 struct CardView: View {
+    let card: MemorizeGame<String>.Card
+    
+    init(_ card: MemorizeGame<String>.Card) {
+        self.card = card
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        let shape = RoundedRectangle(cornerSize: CGSize(width: 15, height:15))
+        ZStack{
+            Group {
+                shape.fill(.white)
+                shape.strokeBorder(lineWidth: 2)
+                Text(card.content)
+                    .font(.system(size: 120))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1, contentMode: .fit)
+            }.opacity(card.isFaceUp ? 1 : 0)
+                //.fill is default for shape
+            shape.opacity(card.isFaceUp ? 0 : 1)
+        }
+        .opacity(!card.isMatched || card.isFaceUp ? 1 : 0)
     }
 }
 
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -86,31 +86,7 @@ struct EmojiMemoryGameView: View {
 
 
 
-//Card view to make it reusable
-struct CardView: View {
-    let card: MemorizeGame<String>.Card
-    
-    init(_ card: MemorizeGame<String>.Card) {
-        self.card = card
-    }
-    
-    var body: some View {
-        let shape = RoundedRectangle(cornerSize: CGSize(width: 15, height:15))
-        ZStack{
-            Group {
-                shape.fill(.white)
-                shape.strokeBorder(lineWidth: 2)
-                Text(card.content)
-                    .font(.system(size: 120))
-                    .minimumScaleFactor(0.01)
-                    .aspectRatio(1, contentMode: .fit)
-            }.opacity(card.isFaceUp ? 1 : 0)
-                //.fill is default for shape
-            shape.opacity(card.isFaceUp ? 0 : 1)
-        }
-        .opacity(!card.isMatched || card.isFaceUp ? 1 : 0)
-    }
-}
+
 
 
 
