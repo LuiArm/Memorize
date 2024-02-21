017cb59eb5c4c8cec1f6ca214a501c224fc42d29
LuiArm <Luisarmendariz24@icloud.com>
2024-02-20 23:39:37 -0700

    pie shape

--- Memorize/Pie.swift
+++ Memorize/Pie.swift
@@ -9,5 +9,14 @@ import SwiftUI
 
 
 struct Pie: Shape {
+    var startAngle: Angle = .zero
+    let endAngle: Angle
+    
+    func path(in rect: CGRect) -> Path {
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        
+        var p = Path()
+    }
+    
     
 }
